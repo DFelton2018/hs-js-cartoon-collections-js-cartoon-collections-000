a2869d8972756945a5ff457af7aa743cf815d5cc
Duane Felton <dfelton2010@gmail.com>
2018-05-23 17:03:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function dwarfRollCall(dwarves) {
+  var numberedDwarves = [];
+  for (var i = 0; i<dwarves.length; i++) {
+   numberedDwarves.push(i+1 + '. ' + dwarves[i]);
+  }
 }
 
 function summonCaptainPlanet(planeteerCalls){
