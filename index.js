77957ac8307d0f7e232807ccb123e474d5b61cce
Duane Felton <dfelton2010@gmail.com>
2018-05-24 16:38:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function dwarfRollCall(dwarves) {
-  var numberedDwarves = [];
-  for (var i = 0; i<dwarves.length; i++) {
-   numberedDwarves.push(i+1 + '. ' + dwarves[i]);
+function dwarfRollCall (dwarves) {
+  var roleCall = [];
+  for (var i = 0; i < dwarves.length; i++) {
+  roleCall.push([i+1] + ". " + dwarves[i] + ' ');
   }
+  return roleCall.join('');
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet (planeteerCalls){
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls (words) {
 }
 
 function findTheCheese (foods) {
