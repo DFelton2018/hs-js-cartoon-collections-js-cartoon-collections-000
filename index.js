74c0fa907040d993b88a7c6d6ac45a103c3448fa
Duane Felton <dfelton2010@gmail.com>
2018-06-12 16:16:14 +0000

    Done.

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ function summonCaptainPlanet (planeteerCalls) {
 function longPlaneteerCalls (words) {
 
   for (var i = 0; i < words.length; i++) {
-  if (words[i].length > 4) {
-  return true;
+    if (words[i].length > 4) {
+      return true;
 } else {
-  return false;
+    return false;
     }
   }
 }
 
 function findTheCheese (foods) {
   for (var i = 0; i < foods.length; i ++)
-  if (foods[i] === 'cheddar') {
-    return foods[i];
+    if (foods[i] === 'cheddar') {
+      return foods[i];
 }
   if(foods[i] !='cheddar') { 
     return 'no cheese!';
